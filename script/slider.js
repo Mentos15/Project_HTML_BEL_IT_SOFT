4137a298259629457c9480352d4c7d71631aed3d
Mentos015 <ermakovich2000@mail.ru>
2020-10-22 19:01:17 +0300

    100%

--- script/slider.js
+++ script/slider.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){
   $('.second-background').slick();
+
+  
+  $('.first').click(function(event){
+    $('.second').removeClass('active');
+    $('.third').removeClass('active');
+    $('.first').toggleClass('active');
+  });
+  $('.second').click(function(event){
+    $('.first').removeClass('active');
+    $('.third').removeClass('active');
+    $('.second').toggleClass('active');
+  });
+  $('.third').click(function(event){
+    $('.first').removeClass('active');
+    $('.second').removeClass('active');
+    $('.third').toggleClass('active');
+  });
+
+
 });
