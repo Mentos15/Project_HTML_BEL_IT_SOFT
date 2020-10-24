08b276d969725aa1a92ad34496f5e3211746f795
Mentos015 <ermakovich2000@mail.ru>
2020-10-24 08:40:14 +0300

    last commit

--- script/slider.js
+++ script/slider.js
@@ -6,16 +6,19 @@ $(document).ready(function(){
     $('.second').removeClass('active');
     $('.third').removeClass('active');
     $('.first').toggleClass('active');
+    $('.our-clients-content-pagination__photo').css('background-image','url(./img/our-clients/4-1.png)')
   });
   $('.second').click(function(event){
     $('.first').removeClass('active');
     $('.third').removeClass('active');
     $('.second').toggleClass('active');
+    $('.our-clients-content-pagination__photo').css('background-image','url(./img/our-clients/pagination-img.png)')
   });
   $('.third').click(function(event){
     $('.first').removeClass('active');
     $('.second').removeClass('active');
     $('.third').toggleClass('active');
+    $('.our-clients-content-pagination__photo').css('background-image','url(./img/our-clients/3-1.png)')
   });
 
 
